a604919eb4b2929326dbdb808b328984f8d94a52
Алексей Шестаков <shestakov_ao@bk.ru>
2023-04-01 14:07:26 +0700

    Добавил текст

--- AutoCAD1/DrawObject.cs
+++ AutoCAD1/DrawObject.cs
@@ -15,6 +15,42 @@ namespace AutoCAD1
 {
     public class DrawObject
     {
+        [CommandMethod("DrawMText")]
+        public void DrawMText()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor edt = doc.Editor;
+
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+                try
+                {
+                    BlockTable bt;
+                    bt = trans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                    BlockTableRecord btr;
+                    btr = trans.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+                    string txt = "AutoCAD111";
+                    Point3d insertPoint = new Point3d(200, 200, 0);
+
+                    using (MText mText = new MText())
+                    {
+                        mText.Contents = txt; //текст
+                        mText.Location = insertPoint; //точка вставки
+                        mText.Height = 23; //пользовательская высота
+                        mText.TextHeight = 30; //Высота текста
+                        btr.AppendEntity(mText);
+                        trans.AddNewlyCreatedDBObject(mText, true);
+                    }
+                    trans.Commit();
+                }
+                catch (System.Exception ex)
+                {
+                    edt.WriteMessage("Error: " + ex.Message);
+                    trans.Abort();
+                }
+        }
+
         [CommandMethod("DrawLine")]
         public void DrawLine()
         {
